4ca50c79d34e8ca848cf85f94cd65585a02d19a5
jlmcd <jonathanlmcd@gmail.com>
2022-07-15 12:00:39 -0600

    confidence meter

--- src/App.js
+++ src/App.js
@@ -2,27 +2,45 @@ import { useState } from 'react';
 import './app.css';
 import generateDummyData from './dummyDataGenerator';
 
-const WHITE = '#E6EBE0';
-const PURPLE = '#301A4B';
-const RED = '#F39A9D';
-const GREEN = '#3F6C51';
-const BLUE = '#15616D';
+const COLORS = {
+  WHITE: '#E6EBE0',
+  PURPLE: '#301A4B',
+  RED: '#F39A9D',
+  GREEN: '#3F6C51',
+  BLUE: '#15616D',
+};
 
 const App = () => {
   const [inputValue, setInputValue] = useState(0);
-  const [results, setResults] = useState({prediction: null, confidence: null});
+  const [results, setResults] = useState({
+    prediction: null,
+    confidence: null,
+  });
 
   const onValueChange = (evt) => {
     evt.preventDefault();
     const newValue = Number(evt.target.value);
     if (Number.isNaN(newValue) || typeof newValue === 'string') return;
-    if (typeof newValue === 'number') setInputValue(newValue);
+    if (typeof newValue === 'number') setInputValue(Math.abs(newValue));
   };
 
   const onSubmit = () => {
     const res = generateDummyData();
     setResults(res);
   };
+
+  const calculateConfidenceStyle = () => {
+    if (!results.prediction || !results.confidence) return null;
+    return {
+      height: `${results.confidence * 100}%`,
+      backgroundColor: COLORS[results.prediction],
+    };
+  };
+
+  const calculateResultColor = () => {
+    if (!results.prediction) return null;
+
+  }
 
   return (
     <div className="anomoly-detection-fe">
@@ -32,8 +50,13 @@ const App = () => {
         <button onClick={onSubmit}>Predict</button>
       </div>
       <div className="thermometer-container">
-        <div className="thermometer__stem"></div>
-        <div className="thermometer__bulb"></div>
+        <div className="thermometer__stem">
+          <div
+            className="thermometer__confidence-measurement"
+            style={calculateConfidenceStyle()}
+          />
+        </div>
+        <div className="thermometer__bulb" style={ calculateResultColor() } />
       </div>
     </div>
   );
