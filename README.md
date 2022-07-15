476fa890e1a4c1f62a51bf29df3a626368121493
jlmcd <jonathanlmcd@gmail.com>
2022-07-15 10:18:35 -0600

    remove fluff

---
+++ README.md
@@ -0,0 +1,6 @@
+# Anomoly Detection FE
+Requires node v14 or higher
+
+`npm i`
+
+`npm start`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="anomoly-detection-fe">
+      <input type="number" />
+    </div>
+  );
+}
+
+export default App;
